36a5e43882b80955609bda692151da5c8746ca70
Davide <pizidavi@gmail.com>
2021-03-18 19:30:31 +0100

    Changed TelegramBot library
    Python-Telegram-Bot instead of Telepot

--- app/view.py
+++ app/view.py
@@ -1,7 +1,7 @@
-from telepot import Bot
-from telepot.loop import MessageLoop
-from telepot.namedtuple import InlineKeyboardButton
 from datetime import datetime, timedelta
+from telegram import InlineKeyboardButton
+from telegram.ext import Updater, Filters
+from telegram.ext import MessageHandler, CallbackQueryHandler
 
 from lib.logger import get_logger
 
@@ -10,10 +10,10 @@ logger = get_logger(__name__)
 
 class View:
 
-    def __init__(self, bot: Bot, handler):
-        self.__bot = bot
-
-        MessageLoop(self.__bot, handler).run_as_thread()
+    def __init__(self, updater: Updater, handler):
+        updater.dispatcher.add_handler(MessageHandler(Filters.chat_type, handler))
+        updater.dispatcher.add_handler(CallbackQueryHandler(handler))
+        updater.start_polling()
         logger.info('TelegramBot started')
 
     @staticmethod
