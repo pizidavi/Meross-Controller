59c5a04fb3c2d0884e637c9f9e514bb2436b3068
pizidavi <49226282+pizidavi@users.noreply.github.com>
2023-05-22 15:50:25 +0200

    Added Wait at start

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import time
 import asyncio
+from datetime import datetime
+
 from app.App import App
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
+
+    timeout_minutes = 5
+    current_time = datetime.now()
+    wait_time = timeout_minutes * 60 - (current_time.minute % timeout_minutes)*60 - current_time.second
+    time.sleep(wait_time)
 
     app = App(loop)
 
